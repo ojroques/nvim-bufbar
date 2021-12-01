dcd25b8b4c7a973fac62c0dfef79aa4d8d9f30b7
Olivier Roques <oroques@fastmail.com>
2021-12-01 14:58:09 +0100

    Update default and one themes

--- lua/bufbar/themes/default.lua
+++ lua/bufbar/themes/default.lua
@@ -1,86 +1,35 @@
 local colors = {
-  black = {gui = '#282C34', cterm = '235', cterm16 = '0'},
-  blue = {gui = '#61AFEF', cterm = '39', cterm16 = '4'},
-  cyan = { gui = '#56B6C2', cterm = '38', cterm16 = '6'},
-  green = {gui = '#98C379', cterm = '114', cterm16 = '2'},
-  grey_comment = {gui = "#5C6370", cterm = "59", cterm16 = "15"},
-  grey_cursor = {gui = "#2C323C", cterm = "236", cterm16 = "8"},
-  grey_menu = {gui = "#3E4452", cterm = "237", cterm16 = "8"},
-  purple = {gui = '#C678DD', cterm = '170', cterm16 = '5'},
-  red = {gui = '#E06C75', cterm = '204', cterm16 = '1'},
-  white = {gui = '#ABB2BF', cterm = '145', cterm16 = '7'},
-  yellow = {gui = '#E5C07B', cterm = '180', cterm16 = '3'},
+  black = {gui = '#181A1F'},
+  blue = {gui = '#61AFEF'},
+  cyan = { gui = '#56B6C2'},
+  green = {gui = '#98C379'},
+  grey = {gui = "#5C6370"},
+  grey_light = {gui = "#31353F"},
+  purple = {gui = '#C678DD'},
+  red = {gui = '#E86671'},
+  white = {gui = '#ABB2BF'},
+  yellow = {gui = '#E5C07B'},
 }
 
 return {
   separator = {
-    normal = {
-      guifg = colors.grey_comment.gui,
-      guibg = colors.grey_cursor.gui,
-      ctermfg = colors.grey_comment.cterm,
-      ctermbg = colors.grey_cursor.cterm,
-    },
-    emphasized = {
-      guifg = colors.grey_menu.gui,
-      guibg = colors.grey_cursor.gui,
-      ctermfg = colors.grey_menu.cterm,
-      ctermbg = colors.grey_cursor.cterm,
-    },
+    normal = {guifg = colors.grey.gui, guibg = colors.grey_light.gui},
+    emphasized = {guifg = colors.grey.gui, guibg = colors.grey_light.gui},
   },
   listed = {
-    inactive = {
-      guifg = colors.green.gui,
-      guibg = colors.grey_cursor.gui,
-      ctermfg = colors.green.cterm,
-      ctermbg = colors.grey_cursor.cterm,
-    },
-    active = {
-      guifg = colors.black.gui,
-      guibg = colors.green.gui,
-      ctermfg = colors.black.cterm,
-      ctermbg = colors.green.cterm,
-    },
+    inactive = {guifg = colors.green.gui, guibg = colors.grey_light.gui},
+    active = {guifg = colors.black.gui, guibg = colors.green.gui},
   },
   modified = {
-    inactive = {
-      guifg = colors.blue.gui,
-      guibg = colors.grey_cursor.gui,
-      ctermfg = colors.blue.cterm,
-      ctermbg = colors.grey_cursor.cterm,
-    },
-    active = {
-      guifg = colors.black.gui,
-      guibg = colors.blue.gui,
-      ctermfg = colors.black.cterm,
-      ctermbg = colors.blue.cterm,
-    },
+    inactive = {guifg = colors.blue.gui, guibg = colors.grey_light.gui},
+    active = {guifg = colors.black.gui, guibg = colors.blue.gui},
   },
   terminal = {
-    inactive = {
-      guifg = colors.red.gui,
-      guibg = colors.grey_cursor.gui,
-      ctermfg = colors.red.cterm,
-      ctermbg = colors.grey_cursor.cterm,
-    },
-    active = {
-      guifg = colors.black.gui,
-      guibg = colors.red.gui,
-      ctermfg = colors.black.cterm,
-      ctermbg = colors.red.cterm,
-    },
+    inactive = {guifg = colors.red.gui, guibg = colors.grey_light.gui},
+    active = {guifg = colors.black.gui, guibg = colors.red.gui},
   },
   tabs = {
-    inactive = {
-      guifg = colors.white.gui,
-      guibg = colors.grey_cursor.gui,
-      ctermfg = colors.white.cterm,
-      ctermbg = colors.grey_cursor.cterm,
-    },
-    active = {
-      guifg = colors.black.gui,
-      guibg = colors.white.gui,
-      ctermfg = colors.black.cterm,
-      ctermbg = colors.white.cterm,
-    },
+    inactive = {guifg = colors.white.gui, guibg = colors.grey_light.gui},
+    active = {guifg = colors.black.gui, guibg = colors.white.gui},
   },
 }
