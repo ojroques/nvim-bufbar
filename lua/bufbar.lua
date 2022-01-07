4ae073209c542bc6910e6daba1312dd64bfd75f0
Olivier Roques <oroques@cisco.com>
2022-01-07 18:32:32 +0100

    Support nil user options

--- lua/bufbar.lua
+++ lua/bufbar.lua
@@ -167,7 +167,9 @@ local function set_bufferline()
 end
 
 function M.setup(user_options)
-  M.options = vim.tbl_extend('force', M.options, user_options)
+  if user_options then
+    M.options = vim.tbl_extend('force', M.options, user_options)
+  end
   set_theme()
   set_hlgroups()
   set_bufferline()
